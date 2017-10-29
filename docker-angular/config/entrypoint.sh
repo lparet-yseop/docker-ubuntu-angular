1871f153f6b1e67114dec2df95ae25bfcddeb861
lparet <lparet@yseop.com>
2017-10-29 15:17:53 +0100

    Check if package.json is present

--- docker-angular/config/entrypoint.sh
+++ docker-angular/config/entrypoint.sh
@@ -3,10 +3,15 @@
 cd /var/www/html
 
 # Install npm packages
+if [ -f package.json ]; then
 npm install
+else
+    echo "You haven't any Angular project in this directory. Please create a volume to plug a project."
+    exit
+fi
 
-# Build 
-ng build
+# Build
+    ng build
 
 echo "/Entrypoint"
 
