1cc3e2388f491b02f175c79cd452e275622416fa
lparet <lparet@yseop.com>
2017-10-29 15:18:08 +0100

    Add environment manage

--- docker-angular/config/entrypoint.sh
+++ docker-angular/config/entrypoint.sh
@@ -4,14 +4,18 @@ cd /var/www/html
 
 # Install npm packages
 if [ -f package.json ]; then
-npm install
+    npm install
 else
     echo "You haven't any Angular project in this directory. Please create a volume to plug a project."
     exit
 fi
 
 # Build
+if [[ -z "${ENVIRONMENT}" ]]; then
+    ng build --environment=${ENVIRONMENT}
+else
     ng build
+fi
 
 echo "/Entrypoint"
 
