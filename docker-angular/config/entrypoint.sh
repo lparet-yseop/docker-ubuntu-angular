c6a5e87dfaeb820f1dfaaf07ff6167fb4a09d242
lparet <lparet@yseop.com>
2017-11-19 00:12:24 +0100

    Use yarn instead of npm

--- docker-angular/config/entrypoint.sh
+++ docker-angular/config/entrypoint.sh
@@ -5,7 +5,7 @@ cd /var/www/html
 
 # Install npm packages
 if [ -f package.json ]; then
-    npm install
+    yarn install
 else
     echo "You haven't any Angular project in this directory. Please create a volume to plug a project."
     exit
